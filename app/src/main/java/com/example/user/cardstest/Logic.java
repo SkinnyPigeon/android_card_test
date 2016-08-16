ace9d421bf2407ad51d88b8bd7aff4d86b7cce74
SkinnyPigeon <philtheskinnypigeon@yahoo.co.uk>
2016-08-16 20:46:23 +0100

    got the cards to deal and working towards seeing a match

---
+++ app/src/main/java/com/example/user/cardstest/Logic.java
@@ -0,0 +1,16 @@
+package com.example.user.cardstest;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by user on 16/08/2016.
+ */
+public class Logic extends AppCompatActivity{
+
+    @Override
+    protected void onCreate( Bundle savedInstanceState ) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_logic);
+    }
+}
--- app/src/main/java/com/example/user/cardstest/TestCards.java
+++ app/src/main/java/com/example/user/cardstest/TestCards.java
@@ -2,6 +2,7 @@ package com.example.user.cardstest;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -15,27 +16,33 @@ public class TestCards extends AppCompatActivity{
     private String dealtCard;
     private ArrayList<String>cards;
     private ArrayList<String>shuffledCards;
+    Button mMatch;
 
 
 
     @Override
     protected void onCreate( Bundle savedInstanceState ) {
-        super.onCreate( savedInstanceState );
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cards);
         TextView cards = (TextView)findViewById( R.id.cardview );
+        mMatch = ( Button )findViewById( R.id.match_button );
 
 
         this.cards = new ArrayList<String>();
 
-        this.cards.add( "AS" );
-        this.cards.add( "AH" );
-        this.cards.add( "AC" );
-        this.cards.add( "AD" );
+        this.cards.add( "A" + getEmijoByUnicode( 0x2660 ) );
+        this.cards.add("A" + getEmijoByUnicode(0x2665));
+        this.cards.add("A" + getEmijoByUnicode(0x2663));
+        this.cards.add( "A" + getEmijoByUnicode( 0x2666 ) );
+        this.cards.add( "K" + getEmijoByUnicode( 0x2660 ) );
+        this.cards.add( "K" + getEmijoByUnicode( 0x2665 ) );
+        this.cards.add( "K" + getEmijoByUnicode( 0x2663 ) );
+        this.cards.add( "K" + getEmijoByUnicode( 0x2666 ) );
 
         this.dealtCard = dealtCard;
 
         shuffle();
-        cards.setText( deal() );
+        cards.setText( deal() + " " + deal() );
 
     }
 
@@ -56,6 +63,11 @@ public class TestCards extends AppCompatActivity{
         pickCard();
         return this.dealtCard;
     }
+
+    public String getEmijoByUnicode(int unicode){
+        return new String(Character.toChars(unicode));
+    }
+
 
 
 
